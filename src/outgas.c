aac0640fd074e0d0c74c42d428e6e2b6e70a34bc
RainerMD <66029459+rainermd@users.noreply.github.com>
2025-03-08 18:25:17 -0500

    Update outgas.c

--- src/outgas.c
+++ src/outgas.c
@@ -1,3 +1,30 @@
+/**
+ * @file    outgas.c
+ * @brief   Add outgassing forces
+ * @author  Rainer Marquardt-Demen
+ * 
+ * @section     LICENSE
+ *
+ *
+ * This file is part of reboundx.
+ *
+ * reboundx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * reboundx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rebound.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The section after the dollar signs gets built into the documentation by a script.  All lines must start with space * space like below.
+ * Tables always must be preceded and followed by a blank line.  See http://docutils.sourceforge.net/docs/user/rst/quickstart.html for a primer on rst.
+ */
+
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
